b80b6ef9d18fac514fc2c0971fc84034f6801025
broylend <broylend@MCD210-PC-02.uccc.uc.edu>
2020-03-05 11:02:18 -0500

    Made InterGalacticComaro class

---
+++ IT2045C-Spring-2020-Midterm-Exam-Basis/src/spacecraft/IntergalacticCamaro.java
@@ -0,0 +1,34 @@
+package spacecraft;
+
+public class IntergalacticCamaro extends Spacecraft {
+	
+	private String color;
+
+	public IntergalacticCamaro(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Launch() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	// Getters and setters
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
